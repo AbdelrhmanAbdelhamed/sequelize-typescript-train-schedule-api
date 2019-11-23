0a33cdf34c92fdae526df78fb9166c011f4be030
Abdelrahman Abdelhamed <abdelrhmanabdelhamed@gmail.com>
2019-11-23 18:12:59 +0200

    DB HOST

--- src/sequelize.ts
+++ src/sequelize.ts
@@ -1,7 +1,7 @@
 import { Sequelize } from 'sequelize-typescript';
 
 export const sequelize = new Sequelize(process.env.DB_NAME!, process.env.DB_USERNAME!, process.env.DB_PASSWORD!, {
-  host:  process.env.HOST,
+  host:  process.env.DB_HOST,
   port: process.env.DB_PORT as any,
   dialect: process.env.DB_DIALECT as any,
   pool: {
