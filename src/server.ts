4357095289599adce593dda36d7586858eef68b4
AbdelrhmanAbdelhamed <abdelrhmanabdelhamed@gmail.com>
2020-02-25 13:59:24 +0200

    change cleaning event to 15 day

--- src/server.ts
+++ src/server.ts
@@ -7,13 +7,13 @@ const port = process.env.PORT || 3000;
 (async () => {
   await sequelize.sync({force: false});
   await sequelize.query(`
-  CREATE EVENT IF NOT EXISTS cleaning ON SCHEDULE EVERY 1 MONTH ENABLE
+  CREATE EVENT IF NOT EXISTS cleaning ON SCHEDULE EVERY 15 DAY ENABLE
   DO
 DELETE train_runs, train_run_police_people, train_run_revisions
 FROM train_runs
 LEFT JOIN train_run_police_people ON train_runs.id = train_run_police_people.train_run_id
 LEFT JOIN train_run_revisions ON train_runs.id = train_run_revisions.id
-WHERE train_runs.day < CURDATE() - INTERVAL 1 MONTH;
+WHERE train_runs.day < CURDATE() - INTERVAL 15 DAY;
 `);
 
   createServer(app)
